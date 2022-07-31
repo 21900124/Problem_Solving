294451965fc44223a17cb786feee9e185d288803
Kim Sohyeon <70438396+21900124@users.noreply.github.com>
2022-07-31 23:33:19 +0900

    Update 좋은 날 싫은 날.cc

--- 백준/Bronze/17211. 좋은 날 싫은 날/좋은 날 싫은 날.cc
+++ 백준/Bronze/17211. 좋은 날 싫은 날/좋은 날 싫은 날.cc
@@ -4,34 +4,35 @@
 using namespace std;
 
 int main(){
-    int n, mood;
-    double gg, gb, bg, bb;
+    int n, mood; // 날 수와 처음 기분을 입력받음
+    double gg, gb, bg, bb; // 좋-좋 / 좋-싫 / 싫-좋 / 싫-싫 확률
 
     cin >> n >> mood >> gg >> gb >> bg >> bb;
 
-    double good[n];
-    double bad[n];
-
+    double goodSum[n]; // 기분이 좋을 확률을 더한 것을 저장할 변수
+    double badSum[n];  // 기분이 나쁠 확률을 더한 것을 저장할 변수
+    
+    // 0으로 초기화
     for(int i = 0; i < n; i++){
-        good[i] = 0;
-        bad[i] = 0;
+        goodSum[i] = 0; 
+        badSum[i] = 0;
     }
 
-    if(mood == 0){
-        good[0] = gg;
-        bad[0] = gb;
+    if(mood == 0){ // 처음 기분이 좋을 경우
+        goodSum[0] = gg; // 첫번째 합은 그 다음날도 기분이 좋을 확률
+        badSum[0] = gb; // 첫번째 합은 그 다음날은 기분이 안좋을 확률
     }
-    else{
-        good[0] = bg; 
-        bad[0] = bb;
+    else{ // 처음 기분이 안좋을 경우
+        goodSum[0] = bg; // 첫번째 합은 그 다음날은 기분이 좋을 확률
+        badSum[0] = bb; // 첫번째 합은 그 다음날도 기분이 안좋을 확률
     }
 
     for(int i = 1; i < n; i++){
-        good[i] = good[i-1] * gg + bad[i-1] * bg;
-        bad[i] = bad[i-1] * bb + good[i-1] * gb;
+        goodSum[i] = goodSum[i-1] * gg + badSum[i-1] * bg; // 이전good - 현재good + 이전bad - 현재good
+        badSum[i] = badSum[i-1] * bb + good[i-1] * gb; // 이전bad - 현재bad + 이전good - 현재bad
     }
-    cout << round(good[n-1] * 1000) << endl;
-    cout << round(bad[n-1] * 1000) << endl;
+    cout << round(goodSum[n-1] * 1000) << endl; // 소수점 첫번재자리에서 반올림
+    cout << round(badSum[n-1] * 1000) << endl;
     
     return 0;
 }
