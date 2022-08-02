dcd60086afc8e6597ff3de5c5aa0d1b27b17fd0a
Kim Sohyeon <70438396+21900124@users.noreply.github.com>
2022-08-02 15:49:39 +0900

    Update 좋은 날 싫은 날.cc

--- 백준/Bronze/17211. 좋은 날 싫은 날/좋은 날 싫은 날.cc
+++ 백준/Bronze/17211. 좋은 날 싫은 날/좋은 날 싫은 날.cc
@@ -29,7 +29,7 @@ int main(){
 
     for(int i = 1; i < n; i++){
         goodSum[i] = goodSum[i-1] * gg + badSum[i-1] * bg; // 이전good - 현재good + 이전bad - 현재good
-        badSum[i] = badSum[i-1] * bb + good[i-1] * gb; // 이전bad - 현재bad + 이전good - 현재bad
+        badSum[i] = badSum[i-1] * bb + goodSum[i-1] * gb; // 이전bad - 현재bad + 이전good - 현재bad
     }
     cout << round(goodSum[n-1] * 1000) << endl; // 소수점 첫번재자리에서 반올림
     cout << round(badSum[n-1] * 1000) << endl;
